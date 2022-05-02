4a4359aeb81c9abe73c6001e09a0ca72b555a915
Thomas Spader <thomas.spader@gmail.com>
2022-05-01 23:14:48 -0400

    independence

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ --std=c++17 main.cpp -I"/home/spader/source/jit_example/include" -I"/home/spader/source/jit_example/include/luajit-2.1" -L"/home/spader/source/jit_example/lib" -lluajit-5.1 -o main
+g++ --std=c++17 main.cpp -I"./include" -I"./include/luajit-2.1" -L"./lib" -lluajit-5.1 -o main
